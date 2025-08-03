dea4e0c54ca276c8d7ddca92ae670086abe22df3
Vedant <vedant28j@gmail.com>
2025-08-03 18:11:03 +0000

    copying the function logic to the controller.js and exporting it to the index.js

---
+++ controller.js
@@ -0,0 +1,9 @@
+export const userNameController = (req,res)=>{
+    const username=req.params.username;
+    res.send(`Welcome ${username}`)
+}
+
+export const searchController = (req,res)=>{
+    const keyword=req.query.keyword;
+    res.send(`Searching for ${keyword}`)
+}
