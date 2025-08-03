cd2a51cd6e5925c55136e133772e610cdbe16153
Vedant <vedant28j@gmail.com>
2025-08-03 18:14:34 +0000

    Comments

--- controller.js
+++ controller.js
@@ -1,9 +1,10 @@
-export const userNameController = (req,res)=>{
-    const username=req.params.username;
-    res.send(`Welcome ${username}`)
-}
+// export const userNameController = (req,res)=>{
+//     const username=req.params.username;
+//     res.send(`Welcome ${username}`)
+// }
 
-export const searchController = (req,res)=>{
-    const keyword=req.query.keyword;
-    res.send(`Searching for ${keyword}`)
-}
+// export const searchController = (req,res)=>{
+//     const keyword=req.query.keyword;
+//     res.send(`Searching for ${keyword}`)
+// }
+
