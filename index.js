6596b0d5c564578d46eb3129da1e382ef8e399e1
Vedant <vedant28j@gmail.com>
2025-08-03 22:59:49 +0000

    Comments

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ const app = express()
 
 const port = 2811
 
-app.use('/welcome',(req,res,next)=>{
-    console.log('A new request received at '+Date.now());
-    next()
-})
-
 // app.METHOD(PATH,HANDLER function);
 // Defining a simple route
 app.get('/',(req,res)=>{
     res.send('Hello, Express')
 })
 
-app.get('/welcome',(req,res)=>{
-    res.send('Welcome to Express App')
-})
+// // MIDDLEWARE
+// app.use('/welcome',(req,res,next)=>{
+//     console.log('A new request received at '+Date.now());
+//     next()
+// })
+
+// app.get('/welcome',(req,res)=>{
+//     res.send('Welcome to Express App')
+// })
 
 // Dynamic Routing
 // app.get('/user/:username',userNameController)
