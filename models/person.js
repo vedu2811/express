ce9e0ac65b3c87a8d26d512d8353e0c052e05bf2
Vedant <vedant28j@gmail.com>
2025-08-10 21:58:54 +0000

    Adding Object to the database, to add empty object: set minimize to false

--- models/person.js
+++ models/person.js
@@ -4,6 +4,7 @@ const personSchema = new mongoose.Schema({
     name: {type: String, required:true},
     age: {type: Number, required:true},
     email: {type: String, required:true, unique:true},
-},{timestamps:true})
+    userOrder : {type:Object, default:{}}
+},{timestamps:true, minimize:false})
 
 export const Person = mongoose.model("Person",personSchema)
