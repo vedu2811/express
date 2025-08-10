f735fb2751af24cd1a7971d0e2b8c792611e4184
Vedant <vedant28j@gmail.com>
2025-08-10 21:42:59 +0000

    Updating the person schema

--- models/person.js
+++ models/person.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    email: String,
+    name: {type: String, required:true},
+    age: {type: Number, required:true},
+    email: {type: String, required:true, unique:true},
 })
 
 export const Person = mongoose.model("Person",personSchema)
