6e8ed2d1c3896e290201334e95bc4cec42f9f290
Vedant <vedant28j@gmail.com>
2025-08-10 21:54:08 +0000

    adding timestamp to person schema, this tells the creation and updation time of the new object

--- models/person.js
+++ models/person.js
@@ -4,6 +4,6 @@ const personSchema = new mongoose.Schema({
     name: {type: String, required:true},
     age: {type: Number, required:true},
     email: {type: String, required:true, unique:true},
-})
+},{timestamps:true})
 
 export const Person = mongoose.model("Person",personSchema)
