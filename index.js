2eab189101920cd8a312658a10094bd18616f52a
Vedant <vedant28j@gmail.com>
2025-08-10 23:37:23 +0000

    Adding a session to calculate number of visits to the server

--- index.js
+++ index.js
@@ -37,6 +37,16 @@ app.get('/',(req,res)=>{
     res.send("Hello, Express")
 })
 
+app.get('/visit',(req,res)=>{
+    if(req.session.page_views){
+        req.session.page_views++;
+        res.send(`You visited this page ${req.session.page_views} times`)
+    }else{
+        req.session.page_views = 1
+        res.send("Welcome to this page for the first time")
+    }
+})
+
 // app.get('/fetch',(req,res)=>{
 //     console.log(req.cookies);
 //     res.send('API called')
